62b4d91f9260049d90b24fa8c36cf9fe72099efe
Alex Caldwell <wacaldwell@gmail.com>
2018-08-07 15:03:47 +0000

    removing wordpress

--- Puppetfile
+++ Puppetfile
@@ -3,7 +3,6 @@ forge 'forge.puppetlabs.com'
 # Forge Modules
 mod 'puppetlabs/ntp', '4.1.0'
 mod 'puppetlabs/stdlib'
-mod 'hunner-wordpress', '1.0.0'
 mod 'puppetlabs-concat', '4.2.1'
 mod 'puppetlabs-mysql', '6.0.0'
 mod 'martasd-mediawiki', '0.3.0'
--- site.pp
+++ site.pp
@@ -1,18 +1,46 @@
-node cavemanbeats-cmbase {
-  include sysprep
-  include syskeys
-  include sysmounts
-  include mediawiki
+node 'wiki' {
+
+  $wikisitename = 'cmbwiki'
+  $wikimetanamespace = 'CMB Wiki'
+  $wikiserver = "http://192.168.1.50"
+  $wikidbserver = 'localhost'
+  $wikidbname = 'wiki'
+  $wikidbuser = 'wiki'
+  $wikidbpassword = 'Ficken2300'
+  $wikiupgradekey = 'puppet'
+
+  class { 'linux': }
+  class { 'mediawiki': }
+
 }
 
-node cmbpi-cmbase {
-  include syskeys
-  include sysprep
+class linux {
+
+  $admintools = ['git', 'nano', 'screen']
+
+  package { $admintools:
+    ensure => 'installed',
+  }
+
+  $ntpservice = $osfamily ? {
+    'redhat' => 'ntpd',
+    'debian' => 'ntp',
+    default  => 'ntp',
+  }
+
+  file { '/info.txt':
+    ensure  => 'present',
+    content => inline_template("Created by Puppet at <%= Time.now %>\n"),
+  }
+
+  package { 'ntp':
+    ensure => 'installed',
+  }
+
+  service { $ntpservice:
+    ensure => 'running',
+    enable => true,
+  }
+
 }
 
-node cavemanbeats-web {
-  include sysprep
-  include syskeys
-  include apache
-  include wordpress
-}
