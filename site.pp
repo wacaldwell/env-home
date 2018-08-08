5d906307ce552ef082e9c2547a7702974870f862
Alex Caldwell <wacaldwell@gmail.com>
2018-08-08 01:28:57 +0000

    more shit

--- site.pp
+++ site.pp
@@ -9,5 +9,18 @@ node 'cavemanbeats-web' {
   $wikidbpassword = 'training'
   $wikiupgradekey = 'puppet'
 
-  class { 'mediawiki': }
+  class { 'mediawiki': 
+  server_name      => 'wiki.localhost',
+  admin_email      => 'wacaldwell@gmail.com',
+  db_root_password => 'Ficken2300',
+  doc_root         => '/var/www/wikis'
+  max_memory       => '1024'
+  }
+  mediawiki::instance { 'my_wiki1':
+  db_password => 'super_long_password',
+  db_name     => 'wiki1',
+  db_user     => 'wiki1_user',
+  port        => '80',
+  ensure      => 'present'
+  }
 }
