fa1b6d8bac4e2dcb1903d6fc0facf594de8afbc0
Alex Caldwell <wacaldwell@gmail.com>
2018-08-08 01:07:54 +0000

    testing

--- site.pp
+++ site.pp
@@ -1,37 +1,14 @@
-node 'cavemanbeats-web' {
+node 'wiki' {
 
-  class { 'linux': }
+  $wikisitename = 'wiki'
+  $wikimetanamespace = 'Wiki'
+  $wikiserver = "http://172.31.0.202"
+  $wikidbserver = 'localhost'
+  $wikidbname = 'wiki'
+  $wikidbuser = 'root'
+  $wikidbpassword = 'training'
+  $wikiupgradekey = 'puppet'
+
   class { 'mediawiki': }
 
 }
-
-class linux {
-
-  $admintools = ['git', 'nano', 'screen']
-
-  package { $admintools:
-    ensure => 'installed',
-  }
-
-  $ntpservice = $osfamily ? {
-    'redhat' => 'ntpd',
-    'debian' => 'ntp',
-    default  => 'ntp',
-  }
-
-  file { '/info.txt':
-    ensure  => 'present',
-    content => inline_template("Created by Puppet at <%= Time.now %>\n"),
-  }
-
-  package { 'ntp':
-    ensure => 'installed',
-  }
-
-  service { $ntpservice:
-    ensure => 'running',
-    enable => true,
-  }
-
-}
-
