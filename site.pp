c49120c1cf3e0522a85caa49e6f5bf786a390e9a
Alex Caldwell <wacaldwell@gmail.com>
2018-08-23 22:23:55 +0000

    removing wiki stuff

--- site.pp
+++ site.pp
@@ -1,28 +1,4 @@
 node 'cavemanbeats-web' {
-
-  $wikisitename = 'wiki'
-  $wikimetanamespace = 'Wiki'
-  $wikiserver = "http://192.168.1.50"
-  $wikidbserver = 'localhost'
-  $wikidbname = 'wiki'
-  $wikidbuser = 'root'
-  $wikidbpassword = 'training'
-  $wikiupgradekey = 'puppet'
-
-  class { 'mediawiki': 
-  server_name      => 'wiki.localhost',
-  admin_email      => 'wacaldwell@gmail.com',
-  db_root_password => 'Ficken2300',
-  doc_root         => '/var/www/wikis'
-  }
-  mediawiki::instance { 'my_wiki1':
-  db_password => 'super_long_password',
-  db_name     => 'wiki1',
-  db_user     => 'wiki1_user',
-  port        => '80',
-  ensure      => 'present'
-  }
-
   include sysfiles
 }
 
