23d34395a5d8b57bb4b77bfbd2748e6f398aef2e
Alex Caldwell <wacaldwell@gmail.com>
2018-08-24 23:52:25 +0000

    adding time to all nodes

--- site.pp
+++ site.pp
@@ -8,12 +8,15 @@ node 'cavemanbeats-web' {
 node 'cavemanbeats-cmbase' {
   include sysprep
   include telegraf
+  include time
 }
 
 node 'puppet-agent-centos' {
   include telegraf
+  include time
 }
 
 node 'sensu' {
   include telegraf
+  include time
 }
