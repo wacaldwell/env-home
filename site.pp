5485fa3ac72d44395e0a78bd7a329626339bc107
Alex Caldwell <wacaldwell@gmail.com>
2018-10-19 14:08:28 -0700

    removing time module

--- site.pp
+++ site.pp
@@ -1,15 +1,12 @@
 node 'cavemanbeats-web' {
   include telegraf
-  include time
 }
 
 node 'cavemanbeats-cmbase' {
   include telegraf
-  include time
 }
 
 node 'cmbpi-cmbase' {
   include telegraf
-  include time
 }
 
