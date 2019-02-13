f04784fb21bff050157f77eeb0630ebc3fd19444
Alex Caldwell <wacaldwell@gmail.com>
2019-02-13 20:50:23 +0000

    adding stuff back to site.pp

--- site.pp
+++ site.pp
@@ -1,2 +1,18 @@
-hiera_include('classes')
+node /web/ {
+  include telegraf
+  include software
+  include hosts
+}
 
+node /base/ {
+  include telegraf
+  include software
+  include hosts
+  include sysmounts
+}
+
+node /nagios/ {
+  include telegraf
+  include hosts
+  include software
+}
