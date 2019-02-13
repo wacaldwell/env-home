55e550f31d8f4953f65c8beb37f0778db9d7a4a2
Alex Caldwell <wacaldwell@gmail.com>
2019-02-13 20:10:40 +0000

    updating site.pp file to only use hiera classes

--- site.pp
+++ site.pp
@@ -1,17 +1,2 @@
-node /web/ {
-  include telegraf
-  include hosts
-  include software
-}
-
-node /base/ {
-  include telegraf
-  include software
-  include hosts
-  include sysmounts
-}
-
-node /nagios/ {
-  include telegraf
-}
+hiera_include('classes')
 
