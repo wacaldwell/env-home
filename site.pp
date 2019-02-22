4e2000fc24140c55320e7acc381badfd5836315e
Alex Caldwell <wacaldwell@gmail.com>
2019-02-22 16:36:24 +0000

    switching to old way again

--- site.pp
+++ site.pp
@@ -1,2 +1,17 @@
-hiera_include('classes')
+node /web/ {
+  include telegraf
+  include software
+  include hosts
+}
 
+node /base/ {
+  include software
+  include hosts
+  include telegraf
+}
+
+node /nagios/ {
+  include telegraf
+  include hosts
+  include software
+}
