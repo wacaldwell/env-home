b0bad8e946302c1a204008e959b18ec9dd28d86a
Alex Caldwell <wacaldwell@gmail.com>
2020-01-11 07:16:08 +0000

    switching site.pp files

--- site.pp
+++ site.pp
@@ -1,2 +1,19 @@
-# doing it
-hiera_include('classes')
+node /web/ {
+  include telegraf
+  include software
+  include hosts
+}
+
+node /base/ {
+  include telegraf
+  include software
+  include hosts
+  include sysmounts
+}
+
+node /nagios/ {
+  include telegraf
+  include hosts
+  include software
+}
+
