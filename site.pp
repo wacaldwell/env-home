3ce37c6c94275f70102b044865fe2d312afc04d2
Alex Caldwell <wacaldwell@gmail.com>
2020-01-16 23:04:22 +0000

    simplying calls

--- site.pp
+++ site.pp
@@ -1,27 +1,5 @@
-node /web/ {
-  include telegraf
-  include software
-  include hosts
-}
-
 node /base/ {
-  include telegraf
-  include software
-  include hosts
-  include sysmounts
-  include duckdns
-  include directories
+  include ntp
 }
 
-node /monstack/ {
-  include telegraf
-  include software
-  include hosts
-}
-
-node /nagios/ {
-  include telegraf
-  include software
-  include hosts
-}
 
