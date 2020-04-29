cef18b8b86254b293f23818d9eb9e718394b719b
Alex Caldwell <wacaldwell@gmail.com>
2020-04-29 23:37:59 +0000

    addig proper node defintion for webhost

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,21 @@
 #hiera_include('classes')
-node /^webhost/ {
-  include role::lamp
-}
+#node /^webhost/ {
+#  include role::lamp
+#}
 # node with default classifications
 #node default {
 #}
-#
+
+node /webhost/ {
+  include keys
+  include hosts
+  include ntp
+  include software
+}
+
 node /base/ {
   include keys
   include hosts
   include software
 }
+
