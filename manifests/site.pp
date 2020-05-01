1b2a9cac41582b2b03d4349cc2c46c44e437fca2
Alex Caldwell <alex.caldwell@xoom.com>
2020-05-01 11:18:41 -0700

    adding lamp role

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,19 +1,19 @@
 #hiera_include('classes')
-#node /^webhost/ {
-#  include role::lamp
-#}
+node /^webhost/ {
+  include role::lamp
+}
 # node with default classifications
 #node default {
 #}
 
-node /webhost/ {
-  include keys
-  include hosts
-  include ntp
-  include software
-  include telegraf
-  include motd
-}
+# node /webhost/ {
+#   include keys
+#   include hosts
+#   include ntp
+#   include software
+#   include telegraf
+#   include motd
+# }
 
 node /base/ {
   include keys
