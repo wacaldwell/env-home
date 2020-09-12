2165159b9cf2a9d87d6d2e08c7c1c80a21954854
Alex Caldwell <wacaldwell@gmail.com>
2020-09-12 11:47:06 -0700

    adding hosts to baseserver

--- site-modules/role/manifests/baseserver.pp
+++ site-modules/role/manifests/baseserver.pp
@@ -3,6 +3,7 @@
 class role::baseserver {
   include ::profile::software
   include ::profile::telegraf
+  include ::profle::hosts
 #  include ::profile::keys
 #  include ::profile::sshdconfig
 #  include ::profile::users
