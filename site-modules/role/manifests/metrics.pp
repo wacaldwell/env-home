c0bf7122c9008dd8b0d696bdc5363d201c02812d
Alex Caldwell <wacaldwell@gmail.com>
2021-01-16 19:43:42 -0800

    fucking around

--- site-modules/role/manifests/metrics.pp
+++ site-modules/role/manifests/metrics.pp
@@ -1,5 +1,10 @@
-# Role for a mectrics server.
+# Role for a metrics server.
 #
 class role::metrics {
+  include ::profile::software
+  include ::profile::telegraf
   include ::profile::hosts
+#  include ::profile::keys
+#  include ::profile::sshdconfig
+#  include ::profile::users
 }
