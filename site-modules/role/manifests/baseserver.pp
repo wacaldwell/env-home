8e05c809aece45b3545a6772af701738465f9511
Alex Caldwell <wacaldwell@gmail.com>
2021-03-29 11:19:09 -0700

    adjusting baseserver role

--- site-modules/role/manifests/baseserver.pp
+++ site-modules/role/manifests/baseserver.pp
@@ -1,10 +1,10 @@
 # Role for a Base server.
 #
 class role::baseserver {
-#  include ::profile::resolv
+# include ::profile::resolv
   include ::profile::software
-#  include ::profile::telegraf
-#  include ::profile::keys
-  include ::profile::sshdconfig
-#  include ::profile::users
+  include ::profile::telegraf
+# include ::profile::keys
+#  include ::profile::sshdconfig
+  include ::profile::users
 }
