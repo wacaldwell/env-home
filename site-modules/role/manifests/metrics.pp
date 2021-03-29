84902e44ecc97e8850b7d9efb8ba041f719a0290
Alex Caldwell <alexcaldwell@paypal.com>
2021-03-29 11:40:15 -0700

    fixintg telegraf hiera data

--- site-modules/role/manifests/metrics.pp
+++ site-modules/role/manifests/metrics.pp
@@ -1,10 +1,6 @@
 # Role for a metrics server.
 #
 class role::metrics {
-#  include ::profile::software
-#  include ::profile::telegraf
+  include ::profile::software
   include ::profile::hosts
-#  include ::profile::keys
-#  include ::profile::sshdconfig
-#  include ::profile::users
 }
