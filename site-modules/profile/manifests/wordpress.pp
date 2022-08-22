707870dd3822f591eaffb75b2d4fd16982307c7d
Alex Caldwell <wacaldwell@gmail.com>
2022-08-21 18:17:16 -0700

    adding wordpress

--- site-modules/profile/manifests/wordpress.pp
+++ site-modules/profile/manifests/wordpress.pp
@@ -2,10 +2,5 @@
 #
 class profile::wordpress {
   class { '::wordpress':
-    wp_owner    => 'wordpress',
-    wp_group    => 'wordpress',
-    db_user     => 'wordpress',
-    db_password => 'hvyH(S%t(\"0\"16',
-    install_dir => '/var/www/html'
   }
 }
