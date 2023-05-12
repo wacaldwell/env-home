5cc5aaac39b649564c1987120d70ae5907bc150f
Alex Caldwell <wacaldwell@gmail.com>
2023-05-12 13:52:36 -0700

    adding syskeys to webhost role

--- site-modules/role/manifests/webserver.pp
+++ site-modules/role/manifests/webserver.pp
@@ -3,6 +3,7 @@
 class role::webserver {
   include ::profile::hosts
   include ::profile::users
+  include ::profile::syskeys
   include ::profile::telegraf
   include ::profile::wordpress
   include ::profile::vsftpd
